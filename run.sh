537e489098889c774318161753c1e74461d1a087
YSL <yangshulin100109@tospur.com>
2025-05-21 17:51:27 +0800

    .

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 if ! command -v uv &> /dev/null; then
   pip install --upgrade pip
   curl -LsSf https://astral.sh/uv/install.sh | sh
+
+  sudo apt-get update
+  sudo apt-get install -y poppler-utils
 fi
 uv sync
 uv pip install -e . --no-cache-dir
